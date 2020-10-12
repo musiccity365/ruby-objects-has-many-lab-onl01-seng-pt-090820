1efb5550314f767400ceffb4d562267dff1e4ab2
musiccity365 <m_sackstein@yahoo.com>
2020-10-11 21:23:34 -0400

    --- Auto-Git Commit ---

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def posts
+        Post.all.select { |post| }
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
